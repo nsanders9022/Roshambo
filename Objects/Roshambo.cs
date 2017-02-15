1cd024da79cc769c94dcc7c3f5921e54b6a14a4c
Derek Villars and Nicole Sanders <derek+nicole@>
2017-02-15 09:35:22 -0800

    add getters, setters, and varibles

--- Objects/Roshambo.cs
+++ Objects/Roshambo.cs
@@ -5,6 +5,46 @@ namespace RoshamboApp
 {
     public class Roshambo
     {
+        private string _userOneInput;
+        private string _userTwoInput;
+        public static Dictionary<string, string> dictname = new Dictionary<string, string>()
+        {
+            {"rock,rock", "no input wins"},
+            {"rock,paper", "input 2 wins with paper"},
+            {"rock,scissors", "input 1 wins with rock"},
+            {"paper,rock", "input 1 wins with paper"},
+            {"paper,paper", "no input wins"},
+            {"paper,scissors", "input 2 wins with scissors"},
+            {"scissors,rock", "input 2 wins with rock"},
+            {"scissors,paper", "input 1 wins with scissors"},
+            {"scissors,scissors", "no input wins"},
+        }
 
+        public Roshambo(string userOneInput, string userTwoInput)
+        {
+            _userOneINput = userOneInput;
+            _userTwoInput = userTwoInput;
+        }
+
+        public void SetInput(string userOneInput, string userTwoInput)
+        {
+            _userOneInput = userOneInput;
+            _userTwoInput = userTwoInput;
+        }
+
+        public string GetUserOneInput()
+        {
+            return _userOneInput;
+        }
+
+        public string GetUserTwoInput()
+        {
+            return _userTwoInput;
+        }
+
+        public string WinChecker()
+        {
+
+        }
     }
 }
